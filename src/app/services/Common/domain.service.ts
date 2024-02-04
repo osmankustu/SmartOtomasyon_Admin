15c2cafc8a3b68c95c0b1c98ecbabd9f29c7a385
Osman KUŞTU <64109752+osmankustu@users.noreply.github.com>
2024-02-05 01:02:50 +0300

    Update domain.service.ts

--- src/app/services/Common/domain.service.ts
+++ src/app/services/Common/domain.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from "@angular/core";
 })
 export class DomainService {
  //local
-  //public domain:String = "https://localhost:7115/api/";
+  public domain:String = "https://localhost:7115/api/";
   //prodduction
-  public domain:String = "https://antalyasmartotomasyon.somee.com/Data/api/";
+  //public domain:String = "";
   constructor() {}
 }
